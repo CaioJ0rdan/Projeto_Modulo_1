888b9a5fecbe6f873b3972dc34dc111bb5d4b2fc
Caio Jordan <114172711+CaioJ0rdan@users.noreply.github.com>
2024-08-18 23:37:05 -0300

    Update User-data.sh

--- User-data.sh
+++ User-data.sh
@@ -1,41 +1,52 @@
 #!/bin/bash
 
-# Atualizando o sistema
-yum update -y
+# Atualizar os pacotes
+sudo yum update -y
 
-# Instalando o Apache
-yum install -y httpd
+# Instalar Apache
+sudo yum install httpd -y
 
-# Criando a página HTML no diretório '/var/www/html/'
-cat << EOF > /var/www/html/index.html
-<!DOCTYPE html>
+# Iniciar o serviço Apache
+sudo systemctl start httpd
+
+# Configurar para iniciar o Apache automaticamente ao inicializar
+sudo systemctl enable httpd
+
+# Criar o diretório para o CSS, caso não exista
+sudo mkdir -p /var/www/html/css
+
+# Criar uma página HTML simples
+sudo bash -c 'echo "<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Servidor Web</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Servidor Web </title>
+    <link rel="stylesheet" href="styles.css" >
 </head>
 <body>
     <div class="container" id="container">
-        <h1>Bem-vindo ao Servidor Web</h1>
-        <h2>
-            Esta é a página HTML referente à atividade de Projeto 03 do módulo 1 do curso de DevOps
-            do Santander Coders.<br>
-            Aluno: Caio Jordan Oliveira de Siqueira
+        <h1>
+            Bem-vindo ao Servidor Web 
+        </h1>
+        <h2> 
+            Esta é página HTML referente à atividade de Projeto 03 do módulo 1 do curso de DevOps
+            do Santander Coders.
+            <br>
+
+            Aluno : Caio Jordan Oliveira de Siqueira
         </h2>
         <h3>
             Este servidor está sendo executado em uma instância Amazon Linux
             configurada automaticamente via User Data utilizando servidor Apache.
         </h3>
-    </div>
+        
 </body>
 </html>
-EOF
+" > /var/www/html/index.html'
 
-# Criando o arquivo CSS
-cat <<EOF > /var/www/html/styles.css
-body {
+# Criar o arquivo CSS
+sudo bash -c 'echo "body {
     margin: 0;
     padding: 0;
     font-family: Arial, sans-serif;
@@ -54,8 +65,7 @@ body {
     border-radius: 10px;
     box-shadow: 0 0 10px rgba(8, 88, 8, 0.5);
 }
-EOF
+" > /var/www/html/css/styles.css'
 
-# Habilitando e iniciando o Apache
-systemctl enable httpd
-systemctl start httpd
+# Reiniciar Apache para garantir que a nova página seja servida
+sudo systemctl restart httpd
